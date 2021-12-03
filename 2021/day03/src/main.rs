78a889aecc7979f7a3fa1fa1f124d96c09899d41
Oskar Persson <oskar.persson@polken.se>
2021-12-03 12:18:52 +0100

    2021 day 03, part 2

--- 2021/day03/src/main.rs
+++ 2021/day03/src/main.rs
@@ -30,32 +30,54 @@ fn part1(entries: &Vec<&str>) -> i32 {
 
 fn part2(entries: &Vec<&str>) -> i32 {
     let str_len = entries[0].len();
-    let mut final_str = String::from("");
-    let mut final_str_inverse = String::from("");
+
+    let mut oxygen_entries = entries.clone();
+    let mut co2_entries = entries.clone();
 
     for idx in 0..str_len {
-        let mut zeros = 0;
-        let mut ones = 0;
-        for e in entries {
-            if e.chars().nth(idx).unwrap() == '0' {
-                zeros += 1;
+        let oxygen_zero_entries: Vec<&str> = oxygen_entries
+            .clone()
+            .into_iter()
+            .filter(|e| e.chars().nth(idx).unwrap() == '0')
+            .collect();
+        let oxygen_one_entries: Vec<&str> = oxygen_entries
+            .clone()
+            .into_iter()
+            .filter(|e| e.chars().nth(idx).unwrap() == '1')
+            .collect();
+
+        if oxygen_entries.len() > 1 {
+            if oxygen_zero_entries.len() > oxygen_one_entries.len() {
+                oxygen_entries = oxygen_zero_entries.clone();
             } else {
-                ones += 1;
+                oxygen_entries = oxygen_one_entries.clone();
             }
         }
-        if zeros > ones {
-            final_str.push('0');
-            final_str_inverse.push('1');
-        } else {
-            final_str.push('1');
-            final_str_inverse.push('0');
+
+        let co2_zero_entries: Vec<&str> = co2_entries
+            .clone()
+            .into_iter()
+            .filter(|e| e.chars().nth(idx).unwrap() == '0')
+            .collect();
+        let co2_one_entries: Vec<&str> = co2_entries
+            .clone()
+            .into_iter()
+            .filter(|e| e.chars().nth(idx).unwrap() == '1')
+            .collect();
+
+        if co2_entries.len() > 1 {
+            if co2_one_entries.len() >= co2_zero_entries.len() {
+                co2_entries = co2_zero_entries.clone();
+            } else {
+                co2_entries = co2_one_entries.clone();
+            }
         }
     }
 
-    let gamma = i32::from_str_radix(&final_str, 2).unwrap();
-    let epsilon = i32::from_str_radix(&final_str_inverse, 2).unwrap();
+    let oxygen_rating = i32::from_str_radix(oxygen_entries[0], 2).unwrap();
+    let co2_rating = i32::from_str_radix(co2_entries[0], 2).unwrap();
 
-    gamma * epsilon
+    oxygen_rating * co2_rating
 }
 
 fn main() {
