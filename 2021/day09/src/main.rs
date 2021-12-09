9eac2a27a6c801cc56917eea4a909b6b830d57b9
Oskar Persson <oskar.persson@polken.se>
2021-12-09 18:10:21 +0100

    2021 day 09, part2

--- 2021/day09/src/main.rs
+++ 2021/day09/src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 
-fn parse_input(contents: &str) -> HashMap<i32, HashMap<i32, i32>> {
-    let mut map: HashMap<i32, HashMap<i32, i32>> = HashMap::new();
+type Grid = HashMap<i32, HashMap<i32, i32>>;
+
+fn parse_input(contents: &str) -> Grid {
+    let mut map: Grid = HashMap::new();
     for (x, l) in contents.lines().enumerate() {
         for (y, c) in l.chars().enumerate() {
             let d = c.to_digit(10).unwrap() as i32;
@@ -17,57 +19,88 @@ fn parse_input(contents: &str) -> HashMap<i32, HashMap<i32, i32>> {
     map
 }
 
-fn part1(contents: &str) -> i32 {
-    let entries = parse_input(contents);
-    let mut risk_level = 0;
+fn get_point(entries: &Grid, x: i32, y: i32) -> Option<i32> {
+    if let Some(e) = entries.get(&x) {
+        return e.get(&y).map(|ee| *ee);
+    }
+    None
+}
 
-    for (i, x) in &entries {
-        for (j, y) in x {
-            if let Some(v) = entries.get(&i).unwrap().get(&(j + 1)) {
-                if y >= v {
-                    continue;
-                }
-            }
-            if let Some(v) = entries.get(&i).unwrap().get(&(j - 1)) {
-                if y >= v {
-                    continue;
-                }
-            }
-            if let Some(Some(v)) = entries.get(&(i + 1)).map(|v| v.get(&j)) {
-                if y >= v {
-                    continue;
-                }
-            }
-            if let Some(Some(v)) = entries.get(&(i + 1)).map(|v| v.get(&(j + 1))) {
-                if y >= v {
-                    continue;
-                }
-            }
-            if let Some(Some(v)) = entries.get(&(i + 1)).map(|v| v.get(&(j - 1))) {
-                if y >= v {
-                    continue;
-                }
-            }
-            if let Some(Some(v)) = entries.get(&(i - 1)).map(|v| v.get(&j)) {
-                if y >= v {
-                    continue;
-                }
-            }
-            if let Some(Some(v)) = entries.get(&(i - 1)).map(|v| v.get(&(j + 1))) {
-                if y >= v {
-                    continue;
-                }
-            }
-            if let Some(Some(v)) = entries.get(&(i - 1)).map(|v| v.get(&(j - 1))) {
-                if y >= v {
-                    continue;
-                }
-            }
-            risk_level += y + 1;
-        }
+fn adjacent_points(entries: &Grid, x: i32, y: i32) -> Vec<(i32, i32, i32)> {
+    let mut points = vec![];
+
+    if let Some(v) = get_point(&entries, x, y + 1) {
+        points.push((x, y + 1, v));
+    }
+    if let Some(v) = get_point(&entries, x, y - 1) {
+        points.push((x, y - 1, v))
+    }
+    if let Some(v) = get_point(&entries, x + 1, y) {
+        points.push((x + 1, y, v))
+    }
+    if let Some(v) = get_point(&entries, x - 1, y) {
+        points.push((x - 1, y, v))
     }
 
-    risk_level
+    points
+}
+
+fn get_low_points(entries: &Grid) -> Vec<(i32, i32, i32)> {
+    let mut points = vec![];
+    for (i, x) in entries {
+        for (j, y) in x {
+            if adjacent_points(&entries, *i, *j)
+                .iter()
+                .all(|(_, _, adj)| adj > y)
+            {
+                points.push((*i, *j, *y));
+            }
+        }
+    }
+    points
+}
+
+fn part1(contents: &str) -> i32 {
+    let entries = parse_input(contents);
+    get_low_points(&entries)
+        .into_iter()
+        .map(|(_, _, r)| r + 1)
+        .sum()
+}
+
+fn basin_points(grid: &Grid, x: i32, y: i32, v: i32, acc: &mut Vec<(i32, i32, i32)>) -> i32 {
+    let higher_points: Vec<(i32, i32, i32)> = adjacent_points(&grid, x, y)
+        .into_iter()
+        .filter(|(_, _, av)| *av > v && *av < 9)
+        .collect();
+
+    acc.append(&mut higher_points.clone());
+
+    let res = higher_points
+        .iter()
+        .map(|(ax, ay, av)| basin_points(grid, *ax, *ay, *av, acc))
+        .count() as i32;
+    res
+}
+
+fn part2(contents: &str) -> i32 {
+    let entries = parse_input(contents);
+    let low_points = get_low_points(&entries);
+
+    let mut basins: Vec<i32> = low_points
+        .iter()
+        .map(|(x, y, v)| {
+            let mut vv = vec![(*x, *y, *v)];
+            basin_points(&entries, *x, *y, *v, &mut vv);
+            vv.sort();
+            vv.dedup_by(|(vvx1, vvy1, _), (vvx2, vvy2, _)| vvx1 == vvx2 && vvy1 == vvy2);
+            vv.len() as i32
+        })
+        .collect();
+
+    basins.sort();
+    basins.reverse();
+    basins.iter().take(3).product()
 }
 
 fn main() {
@@ -75,6 +108,9 @@ fn main() {
 
     let part1 = part1(contents);
     println!("part1: {}", part1);
+
+    let part2 = part2(contents);
+    println!("part2: {}", part2);
 }
 
 #[cfg(test)]
@@ -89,5 +125,15 @@ mod tests {
 8767896789
 9899965678";
         assert_eq!(part1(contents), 15);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let contents = "2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+        assert_eq!(part2(contents), 1134);
     }
 }
