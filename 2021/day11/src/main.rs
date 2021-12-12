4d55f99ec7cbfc98806715f6978a4251e0f2a5b0
Oskar Persson <oskar.persson@tmpl.se>
2021-12-12 20:47:48 +0100

    2021 day 11, part 1

--- 2021/day11/src/main.rs
+++ 2021/day11/src/main.rs
@@ -1,19 +1,27 @@
-type Grid = Vec<Vec<usize>>;
+use std::collections::HashMap;
+
+type Grid = HashMap<usize, HashMap<usize, usize>>;
 
 fn parse_input(contents: &str) -> Grid {
-    contents
-        .lines()
-        .map(|l| {
-            l.chars()
-                .map(|c| c.to_digit(10).unwrap() as usize)
-                .collect()
-        })
-        .collect()
+    let mut map: Grid = HashMap::new();
+    for (x, l) in contents.lines().enumerate() {
+        for (y, c) in l.chars().enumerate() {
+            let d = c.to_digit(10).unwrap() as usize;
+            if let Some(e) = map.get_mut(&x) {
+                e.insert(y, d);
+            } else {
+                let mut xmap: HashMap<usize, usize> = HashMap::new();
+                xmap.insert(y, d);
+                map.insert(x, xmap);
+            }
+        }
+    }
+    map
 }
 
 fn get_point(grid: &Grid, x: usize, y: usize) -> Option<usize> {
-    if let Some(e) = grid.get(x) {
-        return e.get(y).map(|ee| *ee);
+    if let Some(e) = grid.get(&x) {
+        return e.get(&y).map(|ee| *ee);
     }
     None
 }
@@ -24,35 +32,78 @@ fn get_adjacent(grid: &Grid, x: usize, y: usize) -> Vec<(usize, usize, usize)> {
     if let Some(v) = get_point(&grid, x, y + 1) {
         points.push((x, y + 1, v));
     }
-    if let Some(v) = get_point(&grid, x, y - 1) {
-        points.push((x, y - 1, v))
-    }
     if let Some(v) = get_point(&grid, x + 1, y) {
         points.push((x + 1, y, v))
     }
-    if let Some(v) = get_point(&grid, x - 1, y) {
-        points.push((x - 1, y, v))
+    if let Some(v) = get_point(&grid, x + 1, y + 1) {
+        points.push((x + 1, y + 1, v))
+    }
+    if x > 0 && y > 0 {
+        if let Some(v) = get_point(&grid, x - 1, y - 1) {
+            points.push((x - 1, y - 1, v))
+        }
+    }
+    if x > 0 {
+        if let Some(v) = get_point(&grid, x - 1, y) {
+            points.push((x - 1, y, v))
+        }
+        if let Some(v) = get_point(&grid, x - 1, y + 1) {
+            points.push((x - 1, y + 1, v))
+        }
+    }
+    if y > 0 {
+        if let Some(v) = get_point(&grid, x, y - 1) {
+            points.push((x, y - 1, v))
+        }
+        if let Some(v) = get_point(&grid, x + 1, y - 1) {
+            points.push((x + 1, y - 1, v))
+        }
     }
 
     points
 }
 
-fn flash(grid: &mut Grid, pos: (usize, usize, &mut usize), flashes: &mut i32) {}
+fn flash(grid: &mut Grid, x: usize, y: usize, flashes: &mut i32) {
+    *flashes += 1;
+    for adj in get_adjacent(grid, x, y) {
+        increase_energy(grid, adj.0, adj.1, flashes);
+    }
+}
 
-fn part1(contents: &str) -> i32 {
-    let mut grid = parse_input(contents);
+fn increase_energy(grid: &mut Grid, x: usize, y: usize, flashes: &mut i32) -> () {
+    let point = grid.get_mut(&x).unwrap().get_mut(&y).unwrap();
+    *point += 1;
+
+    if *point == 10 {
+        flash(grid, x, y, flashes);
+        let p = grid.get_mut(&x).unwrap().get_mut(&y).unwrap();
+        *p += 1;
+    }
+}
+
+fn run_steps(grid: &mut Grid, steps: i32) -> i32 {
     let mut flashes = 0;
-
-    for _ in 0..100 {
-        for (i, x) in grid.iter_mut().enumerate() {
-            for (j, y) in x.iter_mut().enumerate() {
-                *y += 1;
-                if y > &mut 9 {
-                    flash(&mut grid, (i, j, y), &mut flashes);
+    let grid_size = 10;
+    for _ in 0..steps {
+        for i in 0..grid_size {
+            for j in 0..grid_size {
+                increase_energy(grid, i, j, &mut flashes)
+            }
+        }
+        for (_, x) in grid.iter_mut() {
+            for (_, y) in x.iter_mut() {
+                if *y > 9 {
+                    *y = 0;
                 }
             }
         }
     }
+    flashes
+}
+
+fn part1(contents: &str) -> i32 {
+    let mut grid = parse_input(contents);
+    let flashes = run_steps(&mut grid, 100);
     flashes
 }
 
