14532fa302e8a937da48ea7bfa882bb57162075d
Oskar Persson <oskar.persson@polken.se>
2021-12-15 21:03:13 +0100

    2021 day 15, part 2, WIP

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -58,11 +58,44 @@ fn part1(contents: &str) -> i32 {
     find_shortest_path(&grid, &mut cache, 0 as usize, 0 as usize).unwrap()
 }
 
+fn part2(contents: &str) -> i32 {
+    let grid = parse_input(contents);
+    let mut biggergrid: Vec<Vec<u32>> = vec![];
+    let rmax = grid.len() as i32;
+    let cmax = grid.get(0).unwrap().len() as i32;
+
+    for r in 0..5 {
+        for rr in 0..rmax {
+            let mut row = vec![];
+            for c in 0..5 {
+                for cc in 0..cmax {
+                    let mut v =
+                        (*grid.get(rr as usize).unwrap().get(cc as usize).unwrap() + r + c) % 9;
+                    if v == 0 {
+                        v = 9;
+                    }
+                    row.push(v);
+                }
+            }
+            biggergrid.push(row);
+        }
+    }
+
+    let start = biggergrid.get_mut(0).unwrap().get_mut(0).unwrap();
+    *start = 0;
+
+    let mut cache: HashMap<(usize, usize), i32> = HashMap::new();
+    find_shortest_path(&biggergrid, &mut cache, 0 as usize, 0 as usize).unwrap()
+}
+
 fn main() {
     let contents = include_str!("../input.txt");
 
     let part1 = part1(contents);
     println!("part1: {}", part1);
+
+    let part2 = part2(contents);
+    println!("part2: {}", part2);
 }
 
 #[cfg(test)]
@@ -85,6 +118,15 @@ mod tests {
     }
 
     #[test]
+    fn test_minimal_3() {
+        let contents = "118
+818
+188
+111";
+        assert_eq!(part1(contents), 6);
+    }
+
+    #[test]
     fn test_part_1() {
         let contents = "1163751742
 1381373672
@@ -97,5 +139,20 @@ mod tests {
 1293138521
 2311944581";
         assert_eq!(part1(contents), 40);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let contents = "1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581";
+        assert_eq!(part2(contents), 315);
     }
 }
