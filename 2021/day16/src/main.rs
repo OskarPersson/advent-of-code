8a297448417d9f273ec106dd40770bd32cf0e16a
Oskar Persson <oskar.persson@polken.se>
2021-12-19 18:03:41 +0100

    2021 day 16, part 2

--- 2021/day16/src/main.rs
+++ 2021/day16/src/main.rs
@@ -26,6 +26,68 @@ fn parse_input(contents: &str) -> String {
         .collect()
 }
 
+#[derive(Clone)]
+struct Packet {
+    pub bits: String,
+    sub_packets: Vec<Packet>,
+    type_id: i32,
+}
+
+impl Packet {
+    fn new(s: String, sub_packets: Vec<Packet>) -> Self {
+        Packet {
+            bits: s.clone(),
+            sub_packets,
+            type_id: get_type_id(&s),
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.bits.len()
+    }
+
+    fn literal_value(&self) -> i64 {
+        let chars = self.bits.chars().skip(6).collect::<Vec<char>>();
+        let s = chars
+            .chunks(5)
+            .map(|grp| grp.iter().skip(1).collect::<String>())
+            .collect::<String>();
+        i64::from_str_radix(&s, 2).unwrap()
+    }
+
+    fn value(&self) -> i64 {
+        match self.type_id {
+            0 => self.sub_packets.iter().map(|p| p.value()).sum(),
+            1 => self.sub_packets.iter().map(|p| p.value()).product(),
+            2 => self.sub_packets.iter().map(|p| p.value()).min().unwrap(),
+            3 => self.sub_packets.iter().map(|p| p.value()).max().unwrap(),
+            4 => self.literal_value(),
+            5 => {
+                if self.sub_packets[0].value() > self.sub_packets[1].value() {
+                    1
+                } else {
+                    0
+                }
+            }
+            6 => {
+                if self.sub_packets[0].value() < self.sub_packets[1].value() {
+                    1
+                } else {
+                    0
+                }
+            }
+            7 => {
+                if self.sub_packets[0].value() == self.sub_packets[1].value() {
+                    1
+                } else {
+                    0
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
 fn binary_str_to_int(s: &str) -> i32 {
     i32::from_str_radix(s, 2).unwrap()
 }
@@ -42,19 +104,14 @@ fn get_length_type_id(s: &str) -> i32 {
     binary_str_to_int(&s.chars().skip(6).take(1).collect::<String>())
 }
 
-fn get_literal_value(s: &str) -> i32 {
-    s.chars().skip(6);
-    todo!()
-}
-
-fn get_sub_packets(s: &str) -> (String, Vec<String>) {
+fn exctract_packet(s: &str) -> Packet {
     if get_type_id(s) == 4 {
         let mut pointer = 6;
         loop {
             let group = s.chars().skip(pointer).take(5).collect::<String>();
             pointer += 5;
             if group.starts_with('0') {
-                return (s.chars().take(pointer).collect::<String>(), vec![]);
+                return Packet::new(s.chars().take(pointer).collect(), vec![]);
             }
         }
     }
@@ -74,7 +131,7 @@ fn get_sub_packets(s: &str) -> (String, Vec<String>) {
 
         while pointer2 < sub_packets_length {
             let sub_str = s.chars().skip(pointer + pointer2).collect::<String>();
-            let (sub_packet, _) = get_sub_packets(&sub_str);
+            let sub_packet = exctract_packet(&sub_str);
             let sub_packet_len = sub_packet.clone().len();
             packets.push(sub_packet.clone());
             pointer2 += sub_packet_len;
@@ -88,12 +145,12 @@ fn get_sub_packets(s: &str) -> (String, Vec<String>) {
         pointer += 11;
         while packets.len() < number_of_sub_packets {
             let sub_str = s.chars().skip(pointer).collect::<String>();
-            let (sub_packet, _) = get_sub_packets(&sub_str);
+            let sub_packet = exctract_packet(&sub_str);
             packets.push(sub_packet.clone());
             pointer += sub_packet.len();
         }
     }
-    return (s.chars().take(pointer).collect(), packets);
+    Packet::new(s.chars().take(pointer).collect(), packets)
 }
 
 fn get_packet_version_sum(s: &str) -> i32 {
@@ -103,11 +160,11 @@ fn get_packet_version_sum(s: &str) -> i32 {
     match type_id {
         4 => version,
         _ => {
-            let (_, sub_packets) = get_sub_packets(s);
+            let sub_packets = exctract_packet(s).sub_packets;
             version
                 + sub_packets
                     .iter()
-                    .map(|p| get_packet_version_sum(p))
+                    .map(|p| get_packet_version_sum(&p.bits))
                     .sum::<i32>()
         }
     }
@@ -118,11 +175,20 @@ fn part1(contents: &str) -> i32 {
     get_packet_version_sum(&s)
 }
 
+fn part2(contents: &str) -> i64 {
+    let s = parse_input(contents);
+    let packet = exctract_packet(&s);
+    packet.value()
+}
+
 fn main() {
     let contents = include_str!("../input.txt");
 
     let part1 = part1(contents);
     println!("part1: {}", part1);
+
+    let part2 = part2(contents);
+    println!("part2: {}", part2);
 }
 
 #[cfg(test)]
@@ -143,11 +209,6 @@ mod tests {
     #[test]
     fn test_part_1_type_id() {
         assert_eq!(get_type_id("110100101111111000101000"), 4);
-    }
-
-    #[test]
-    fn test_part_1_literal_value() {
-        assert_eq!(get_literal_value("110100101111111000101000"), 2021);
     }
 
     #[test]
@@ -184,5 +245,53 @@ mod tests {
     fn test_part_1_example_4() {
         let contents = "A0016C880162017C3686B18A3D4780";
         assert_eq!(part1(contents), 31);
+    }
+
+    #[test]
+    fn test_part_2_example_1() {
+        let contents = "C200B40A82";
+        assert_eq!(part2(contents), 3);
+    }
+
+    #[test]
+    fn test_part_2_example_2() {
+        let contents = "04005AC33890";
+        assert_eq!(part2(contents), 54);
+    }
+
+    #[test]
+    fn test_part_2_example_3() {
+        let contents = "880086C3E88112";
+        assert_eq!(part2(contents), 7);
+    }
+
+    #[test]
+    fn test_part_2_example_4() {
+        let contents = "CE00C43D881120";
+        assert_eq!(part2(contents), 9);
+    }
+
+    #[test]
+    fn test_part_2_example_5() {
+        let contents = "D8005AC2A8F0";
+        assert_eq!(part2(contents), 1);
+    }
+
+    #[test]
+    fn test_part_2_example_6() {
+        let contents = "F600BC2D8F";
+        assert_eq!(part2(contents), 0);
+    }
+
+    #[test]
+    fn test_part_2_example_7() {
+        let contents = "9C005AC2F8F0";
+        assert_eq!(part2(contents), 0);
+    }
+
+    #[test]
+    fn test_part_2_example_8() {
+        let contents = "9C0141080250320F1802104A08";
+        assert_eq!(part2(contents), 1);
     }
 }
