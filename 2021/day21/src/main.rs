0deda7bea2f0d94e639c09dbac2ca964611ee176
Oskar Persson <oskar.persson@polken.se>
2022-01-03 00:16:36 +0100

    2021 day 21, part 2

--- 2021/day21/src/main.rs
+++ 2021/day21/src/main.rs
@@ -1,13 +1,17 @@
-fn parse_input(contents: &str) -> (i32, i32) {
+use std::collections::HashMap;
+
+type Cache = HashMap<(i64, i64, i64, i64, bool), (i64, i64)>;
+
+fn parse_input(contents: &str) -> (i64, i64) {
     let p1 = contents
         .lines()
-        .nth(0)
+        .next()
         .unwrap()
         .chars()
         .last()
         .unwrap()
         .to_digit(10)
-        .unwrap() as i32;
+        .unwrap() as i64;
     let p2 = contents
         .lines()
         .nth(1)
@@ -16,11 +20,11 @@ fn parse_input(contents: &str) -> (i32, i32) {
         .last()
         .unwrap()
         .to_digit(10)
-        .unwrap() as i32;
+        .unwrap() as i64;
     (p1, p2)
 }
 
-fn part1(contents: &str) -> i32 {
+fn part1(contents: &str) -> i64 {
     let (mut p1_space, mut p2_space) = parse_input(contents);
     let (mut p1_score, mut p2_score) = (0, 0);
     let mut die_throws = 0;
@@ -56,11 +60,91 @@ fn part1(contents: &str) -> i32 {
     p1_score.min(p2_score) * die_throws
 }
 
+fn quantum_roll(
+    p1_space: i64,
+    p1_score: i64,
+    p2_space: i64,
+    p2_score: i64,
+    p1: bool,
+    cache: &mut Cache,
+) -> (i64, i64) {
+    if p1_score >= 21 {
+        return (1, 0);
+    }
+    if p2_score >= 21 {
+        return (0, 1);
+    }
+
+    let cache_key = (p1_space, p1_score, p2_space, p2_score, p1);
+
+    if let Some(e) = cache.get(&cache_key) {
+        return *e;
+    }
+
+    let mut p1_total = 0;
+    let mut p2_total = 0;
+
+    for i in 1..=3 {
+        for j in 1..=3 {
+            for k in 1..=3 {
+                let val = i + j + k;
+                let (p1_wins, p2_wins) = if p1 {
+                    let mut new_space = (p1_space + val) % 10;
+                    if new_space == 0 {
+                        new_space = 10;
+                    }
+                    quantum_roll(
+                        new_space,
+                        p1_score + new_space,
+                        p2_space,
+                        p2_score,
+                        !p1,
+                        cache,
+                    )
+                } else {
+                    let mut new_space = (p2_space + val) % 10;
+                    if new_space == 0 {
+                        new_space = 10;
+                    }
+                    quantum_roll(
+                        p1_space,
+                        p1_score,
+                        new_space,
+                        p2_score + new_space,
+                        !p1,
+                        cache,
+                    )
+                };
+                p1_total += p1_wins;
+                p2_total += p2_wins;
+            }
+        }
+    }
+
+    cache.insert(cache_key, (p1_total, p2_total));
+
+    (p1_total, p2_total)
+}
+
+fn part2(contents: &str) -> i64 {
+    let (p1_space, p2_space) = parse_input(contents);
+    let (p1_score, p2_score): (i64, i64) = (0, 0);
+
+    let mut cache: Cache = HashMap::new();
+
+    let (p1, p2) = quantum_roll(p1_space, p1_score, p2_space, p2_score, true, &mut cache);
+
+    p1.max(p2)
+}
+
 fn main() {
     let contents = include_str!("../input.txt");
 
     let part1 = part1(contents);
     println!("part1: {}", part1);
+
+    let part2 = part2(contents);
+    println!("part2: {}", part2);
 }
 
 #[cfg(test)]
@@ -75,6 +159,17 @@ mod tests {
 Player 2 starting position: 8"
             ),
             739785
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part2(
+                "Player 1 starting position: 4
+Player 2 starting position: 8"
+            ),
+            444356092776315
         );
     }
 }
