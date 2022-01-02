238652c34808bfc942ec86dabaa31b881319f239
Oskar Persson <oskar.persson@polken.se>
2022-01-02 14:59:32 +0100

    2021 day 20, part 2

--- 2021/day20/src/main.rs
+++ 2021/day20/src/main.rs
@@ -10,15 +10,16 @@ fn parse_input(contents: &str) -> (Vec<Vec<char>>, String) {
 
 fn get_algo_idx_for_pixel(input: &Vec<Vec<char>>, i: i32, j: i32) -> i32 {
     let mut s = String::from("");
+    let edge_char = input[0][0];
     for x in [-1, 0, 1] {
         match i + x {
             n if n > -1 => {
                 for y in [-1, 0, 1] {
                     match j + y {
                         n if n > -1 => {
-                            let c = input
-                                .get((i + x) as usize)
-                                .map_or(&'.', |l| l.get((j + y) as usize).unwrap_or(&'.'));
+                            let c = input.get((i + x) as usize).map_or(&edge_char, |l| {
+                                l.get((j + y) as usize).unwrap_or(&edge_char)
+                            });
                             s.push(match c {
                                 '.' => '0',
                                 '#' => '1',
@@ -26,13 +27,21 @@ fn get_algo_idx_for_pixel(input: &Vec<Vec<char>>, i: i32, j: i32) -> i32 {
                             });
                         }
                         _ => {
-                            s.push('0');
+                            s.push(match edge_char {
+                                '.' => '0',
+                                '#' => '1',
+                                _ => unreachable!(),
+                            });
                         }
                     }
                 }
             }
             _ => {
-                s.push('0');
+                s.push(match edge_char {
+                    '.' => '0',
+                    '#' => '1',
+                    _ => unreachable!(),
+                });
             }
         }
     }
@@ -40,7 +49,7 @@ fn get_algo_idx_for_pixel(input: &Vec<Vec<char>>, i: i32, j: i32) -> i32 {
 }
 
 fn enhance(input: &Vec<Vec<char>>, algorithm: &str) -> Vec<Vec<char>> {
-    let input = extend(input);
+    let input = extend(input, input[0][0]);
     let mut output = vec![];
     for i in 0..input.len() {
         let mut l = vec![];
@@ -50,50 +59,26 @@ fn enhance(input: &Vec<Vec<char>>, algorithm: &str) -> Vec<Vec<char>> {
         }
         output.push(l);
     }
-    println!("");
-    for i in &output {
-        for j in i {
-            print!("{}", j);
-        }
-        println!("");
-    }
     output
 }
 
-fn extend(input: &Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let c = match input[0][0] {
-        '#' => '#',
-        '.' => '.',
-        _ => unreachable!(),
-    };
-    let line_len = input[0].len() + 6;
-    let mut output = vec![vec![c; line_len]; 3];
+fn extend(input: &Vec<Vec<char>>, c: char) -> Vec<Vec<char>> {
+    let line_len = input[0].len() + 2;
+    let mut output = vec![vec![c; line_len]; 1];
     for i in 0..input.len() {
         let mut l = vec![];
         l.extend(input[i].iter());
         l.insert(0, c);
-        l.insert(0, c);
-        l.insert(0, c);
-        l.push(c);
-        l.push(c);
         l.push(c);
         output.push(l);
     }
     output.push(vec![c; line_len]);
-    output.push(vec![c; line_len]);
-    output.push(vec![c; line_len]);
-    println!("");
-    for i in &output {
-        for j in i {
-            print!("{}", j);
-        }
-        println!("");
-    }
     output
 }
 
 fn part1(contents: &str) -> i32 {
     let (input, algorithm) = parse_input(contents);
+    let input = extend(&input, '.');
     let output = enhance(&enhance(&input, &algorithm), &algorithm);
 
     output
@@ -104,6 +89,7 @@ fn part1(contents: &str) -> i32 {
 
 fn part2(contents: &str) -> i32 {
     let (input, algorithm) = parse_input(contents);
+    let input = extend(&input, '.');
     let mut output = input;
     for _ in 0..50 {
         output = enhance(&output, &algorithm);
