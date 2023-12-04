f44f0feb4365f742ec5862316429135dbcf8be2d
Oskar Persson <oskar.persson@polken.se>
2023-12-04 07:57:23 +0100

    2023 day 04

---
+++ 2023/day04/Program.cs
@@ -0,0 +1,57 @@
+﻿var lines = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt")).ToList();
+
+var part1 = lines.Select((l, i) =>
+{
+    var split = l.Split(" | ");
+    var winners = split[0].Replace("  ", " ").Split(": ")[1].Split(" ").Select(x => int.Parse(x)).ToList();
+    var mine = split[1].Replace("  ", " ").Split(" ").Where(x => !String.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
+
+    var points = 0;
+    foreach (var _ in mine.Where(n => winners.Contains(n)))
+    {
+        if (points == 0)
+        {
+            points++;
+                
+        }
+        else
+        {
+            points *= 2;
+        }
+    }
+    return points;
+}).Sum();
+
+var map = new Dictionary<int, int>();
+
+int p2(List<string> lines)
+{
+    for (var i = 0; i < lines.Count; i++)
+    {
+        map[i] = 0;
+    }
+
+    for (int i = 0; i < lines.Count; i++)
+    {
+        var split = lines[i].Split(" | ");
+        var winners = split[0].Replace("  ", " ").Split(": ")[1].Split(" ").Select(x => int.Parse(x)).ToList();
+        var mine = split[1].Replace("  ", " ").Split(" ").Where(x => !String.IsNullOrWhiteSpace(x))
+            .Select(x => int.Parse(x)).ToList();
+
+
+        map[i] += 1;
+        var ww = mine.Where(m => winners.Contains(m)).ToList();
+
+        for (var k = 0; k < ww.Count(); k++)
+        {
+            map[i+k+1] += map[i];
+        }
+    }
+    return map.Select(m => m.Value).Sum();
+}
+
+
+var part2 = p2(lines);
+
+Console.WriteLine($"part1: {part1}");
+Console.WriteLine($"part2: {part2}");
