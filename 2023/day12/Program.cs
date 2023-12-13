c7a67c0b1f7ecb415ddcccbd4546ae9d7c6ca291
Oskar Persson <oskar.persson@polken.se>
2023-12-13 20:56:01 +0100

    wip

--- 2023/day12/Program.cs
+++ 2023/day12/Program.cs
@@ -5,7 +5,7 @@ var lines = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory
 var sum = 0;
 for (var lineidx = 0; lineidx < lines.Count; lineidx++)
 {
-    var localsum = 0;
+    var localsum = 1;
     var line = lines[lineidx];
 
     var line1 = line.Split(" ")[0];
@@ -24,32 +24,117 @@ for (var lineidx = 0; lineidx < lines.Count; lineidx++)
 
     var records = line.Split(" ")[0].ToCharArray().ToList();
     var groups = line.Split(" ")[1].Split(",").Select(s => int.Parse(s)).ToList();
+
+    var l = line1;
     
+    var j = 0;
+    var grpidx = 0;
 
-    var pattern = "^\\.*";
+    var pattern = @"^[^#.]*(#+)";
+    var pattern2 = @"^[^#.]*(#+\?+)";
 
-    foreach (var grp in groups)
+    while (groups.Count() > 0)
     {
-        pattern += $"#{{{grp}}}\\.+";
-    }
-
-    pattern += "$";
-
-    pattern = pattern.Replace("+$", "*$");
-
-
-    var vars = GetVariations(line.Split(" ")[0]);
-
-    foreach (var v in vars)
-    {
-        if (Regex.IsMatch(v, pattern))
+        var match = Regex.Match(l, pattern);
+        if (match.Groups.Count == 2 && match.Groups[1].Length == groups[0])
         {
-            localsum++;
-            sum++;
+            // if the first set of # matches the length of the next group, skip those # characters.
+            // Also, skip the next coming character to leave a space for the next group
+            l = l.Substring(match.Groups[1].Index + match.Groups[1].Length + 1);
+            groups.RemoveAt(0);
+            continue;
         }
+        else
+        {
+            break;
+        }
+
+        /*
+        var match2 = Regex.Match(l, pattern2);
+
+        if (match2.Groups.Count == 2 && match2.Groups[1].Length == groups[grpidx])
+        {
+            l = l.Substring(match2.Groups[1].Index + match2.Groups[1].Length + 1);
+            grpidx++;
+            continue;
+        }
+        */
+        
+
     }
-    Console.WriteLine(localsum);
+    
+    
+    localsum += GetCombinations(groups, l);
+
+    int GetCombinations(List<int> groups, string substring)
+    {
+        var offset = 0;
+        var matchSuccess = false;
+
+        if (groups.Count == 0)
+        {
+            return 0;
+        }
+
+        if (groups.Count == 1)
+        {
+            while (offset == 0 || matchSuccess)
+            {
+                var ll = substring.Substring(offset);
+                offset++;
+                
+                
+                var pattern3 = $"^([#,\\?]{{{groups[0]}}})";
+                var match3 = Regex.Match(ll, pattern3);
+                matchSuccess = match3.Success;
+            }
+            return Math.Max(offset - 1, 0);
+        }
+
+        var combos = 0;
+        var subSum = 0;
+
+        while (offset == 0 || combos > 1)
+        {
+
+            var ll = substring.Substring(offset);
+            offset++;
+            
+            var pattern3 = $"^([#,\\?]{{{groups[0]}}})";
+            var match3 = Regex.Match(ll, pattern3);
+            if (match3.Success)
+            {
+                var g2 = groups.Skip(1).ToList();
+                combos = GetCombinations(g2, ll.Substring(match3.Index + match3.Length + 1));
+                subSum += combos;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return offset * subSum;
+    }
+
+    /*
+    while (grpidx < groups.Count())
+    {
+        var grp= groups[grpidx];
+
+        var record = records[j];
+        while (record == '.')
+        {
+            j++;
+            record = records[j];
+        }
+
+    }
+    */
+    
+    
     Console.WriteLine();
+    sum += localsum;
 }
 
 
