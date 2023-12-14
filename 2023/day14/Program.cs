0a7af715db32d99959c3dec7d96a1b3259924f05
Oskar Persson <oskar.persson@polken.se>
2023-12-14 21:56:37 +0100

    2023 day 14, part 1

---
+++ 2023/day14/Program.cs
@@ -0,0 +1,40 @@
+﻿var lines = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt")).Select(l => l.ToCharArray()).ToList();
+
+var load = 0;
+for (var r = 0; r < lines.Count; r++)
+{
+    for (var c = 0; c < lines[0].Length; c++)
+    {
+        var ch = lines[r][c];
+        
+        if (ch != 'O')
+        {
+            continue;
+        }
+
+        var rr = r;
+
+        while (rr >= 0)
+        {
+            if (rr == 0)
+            {
+                break;
+            }
+
+            var chAbove = lines[rr-1][c];
+            if (chAbove == '.')
+            {
+                lines[rr-1][c] = 'O';
+                lines[rr][c] = '.';
+                rr -= 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        load += lines.Count - rr;
+    }
+}
+
+Console.WriteLine($"part1: {load}");
